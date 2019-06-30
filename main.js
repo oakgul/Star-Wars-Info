e2506fbfae57440b814de198611a3c7668a592cf
ORHAN AKGÜL <orhan.akgull@gmail.com>
2019-06-30 06:17:09 +0300

    star wars film info kısmı tamamlandı

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const col = document.querySelectorAll('.col');
+const movies = document.querySelectorAll('.episode');
 const output = document.getElementById('output');
 
-yaz = (element) => fetch(`https://swapi.co/api/people/${element.path[0].id}/`)
+getChar = (element) => fetch(`https://swapi.co/api/people/${element.path[0].id}/`)
   .then(response => response.json())
   .then(data => update(data));
 
 update = (data) => {
-  console.log(data);
   output.style.display = 'block';
   output.innerHTML = `<b>Name :</b> <mark>${data.name}</mark> <br> <b>Height :</b> ${data.height} <br> <b>Birth Year :</b> ${data.birth_year} <br> <b>Hair Color :</b> ${data.hair_color} <br>
   <b>Skin Color :</b> ${data.skin_color} <br> <b>Mass :</b> ${data.mass}   <br> <b>Created :</b> ${data.created} <br> <b>Edited :</b> ${data.edited} <br>
@@ -14,4 +14,19 @@ update = (data) => {
   <b>Species :</b> ${data.species} <br> <b>Starships :</b> ${data.starships} <br> <b>Vehicles :</b> ${data.vehicles}`;
 }
 
-col.forEach(element => element.addEventListener('click',yaz));
+col.forEach(element => element.addEventListener('click',getChar));
+
+// MOVIE SIDE
+getEpisode = (element) => fetch(`https://swapi.co/api/films/${element.path[0].id}/`)
+  
+  .then(response => response.json())
+  .then(episode => updateEpisode(episode));
+
+updateEpisode = (episode) => {
+  output.style.display = 'block';
+  output.innerHTML = `<b>Title :</b> <mark>${episode.title}</mark> <br> <b>Episode ID :</b> ${episode.episode_id} <br> <b>Opening Crawl :</b> ${episode.opening_crawl} <br> <b>Director :</b> ${episode.director} <br>
+  <b>Created :</b> ${episode.created} <br> <b>Edited :</b> ${episode.edited}   <br> <b>Producer :</b> ${episode.producer} <br> <b>Characters :</b> ${episode.characters} <br>
+  <b>Release Date :</b> ${episode.release_date} <br> <b>Species :</b> ${episode.species} <br> <b>Starships :</b> ${episode.starships} <br> <b>Vehicles :</b> ${episode.vehicles} <br> <b>Planets :</b> ${episode.planets}`;
+}
+
+movies.forEach(element => element.addEventListener('click',getEpisode));
